449da815832aa87e377d7df051b0f10e0a5e2443
Paloudi <37347063+Paloudi@users.noreply.github.com>
2021-12-20 16:58:30 +0100

    merge dev (#20)

--- projet_dev_ci/urls.py
+++ projet_dev_ci/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path
 from .controller import administrator
 from .controller import user
 from .controller import home
-from django.urls import path
 
 urlpatterns = [
     path('', home.home),
