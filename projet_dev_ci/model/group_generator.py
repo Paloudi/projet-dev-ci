a95d000239c2064f201c37bf6dee6552c974faef
Fred Pouchan <frederic.pouchan@gmail.com>
2022-01-17 19:42:14 +0100

    Added getters & setters + modified some of the previous changes

--- projet_dev_ci/model/group_generator.py
+++ projet_dev_ci/model/group_generator.py
@@ -5,6 +5,7 @@ Module generating groups
 """
 
 import string
+from projet_dev_ci.model import user
 from projet_dev_ci.model.group import Group
 from projet_dev_ci.model.user import User
 from typing import List
@@ -17,7 +18,7 @@ class group_generator:
     """
     # region Constructors
 
-    def __init__(self, users_number : int, groups_number : int, last_param : string) -> None:
+    def __init__(self, users_list : List[User], groups_number : int, last_param : string) -> None:
         """
         Creates a new Group_generator object with a given number of users and number of groups
         :param users_number: The amount of users
@@ -28,13 +29,16 @@ class group_generator:
         :type last_param: string
         """
         super().__init__()
-        if users_number and isinstance(users_number, int) and users_number > 0 and \
+        if users_list and isinstance(users_list, List[User]) and len(users_list) > 0 and \
             groups_number and isinstance(groups_number, int) and groups_number > 0 and \
             last_param and isinstance(last_param, string) and (last_param == "LAST_MIN" or last_param == "LAST_MAX"):
 
-            self.__users_number = users_number
-            self.__groups_number = groups_number
+            self.__users_list = users_list
+            self.__users_number = len(self.__users_list)
+            self.__groups_list = List[Group]
+            self.__groups_number = len(self.__list_groups)
             self.__last_param = last_param
+            
         else:
             raise ValueError("Parameters are invalid")
 
@@ -50,6 +54,14 @@ class group_generator:
             """
             return self.__last_param
 
+    def get_users_list(self) -> List[User]:
+            """
+            Returns the list of the users
+            :return: the list of the users
+            :rtype: List[User]
+            """
+            return self.__users_list
+
     def get_users_number(self) -> int:
             """
             Returns the number of users
@@ -57,6 +69,14 @@ class group_generator:
             :rtype: int
             """
             return self.__users_number
+
+    def get_groups_list(self) -> List[Group]:
+            """
+            Returns the list of the groups
+            :return: the list of the groups
+            :rtype: List[Group]
+            """
+            return self.__groups_list
 
     def get_groups_number(self) -> int:
             """
@@ -76,11 +96,23 @@ class group_generator:
         """
         self.__last_param = last_param
 
+    def set_users_list(self, users_list: List[User]):
+        """
+        set the list of Users
+        """
+        self.__users_list = users_list
+
     def set_users_number(self, users_number: int):
         """
         set the number of users
         """
         self.__users_number = users_number
+
+    def set_groups_list(self, groups_list: List[Group]):
+        """
+        set the list of Groups
+        """
+        self.__groups_list = groups_list
 
     def set_groups_number(self, groups_number: int):
         """
@@ -93,11 +125,26 @@ class group_generator:
     # region methods
 
     def last_group_gestion(self):
-        if self.__users_number % self.__groups_number != 0:
+        """
+        Group gestion 
+        TODO : A modifier
+        """
+        if self.__users_number % self.__groups_number == 0:
             return
-        if group.get_last_param == "LAST_MIN":
+        if self.get_last_param == "LAST_MIN":
             print("blabla")
-        if group.get_last_param == "LAST_MAX":
+        if self.get_last_param == "LAST_MAX":
             print("blabla")
+
+    def add_to_group(self, new_user: User, group: Group)  -> None:
+        """
+        Add a user to an existing group
+        :param new_user: user to add in the existing group
+        :param group: group which the user will be added to 
+        :type new_user: User
+        :type group: Group
+        """
+        group.add_user(new_user)
+
 
     #endrehion
