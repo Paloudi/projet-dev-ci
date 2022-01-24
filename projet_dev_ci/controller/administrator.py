258fcb098433f26961536e46841db75d9af6cc8b
Paloudi <quentinsb@free.fr>
2022-01-24 19:01:29 +0100

    il est vide

--- projet_dev_ci/controller/administrator.py
+++ projet_dev_ci/controller/administrator.py
@@ -7,12 +7,16 @@ Controller of the administrator route.
 
 from datetime import datetime
 from projet_dev_ci.view import administrator as v
+from projet_dev_ci.models import Group
 
 
 def home(request):
     """
     Handles request of the administrator route.
     """
+    request
     now = datetime.now()
+    grp = Group(5)
+    grp.save()
     return v.home(now)
 
--- projet_dev_ci/view/user.py
+++ projet_dev_ci/view/user.py
@@ -6,3 +6,4 @@ from django.http import HttpResponse
 def home(time):
     html = "<html><body>It is now %s.</body></html>" % time
     return HttpResponse(html)
+
