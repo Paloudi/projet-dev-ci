62ec8cd697c5cf4dee10a1eafdcceae764ba7171
Paloudi <quentinsb@free.fr>
2022-01-28 23:00:48 +0100

    trying something else

--- projet_dev_ci/test/test_selenium.py
+++ projet_dev_ci/test/test_selenium.py
@@ -1,23 +1,29 @@
 import socket
+import unittest
 
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-import unittest
+from webdriver.chrome import ChromeDriverManager
+
+
+def driver_initializer():
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
+    driver.maximize_window()
+    return driver
 
 
 class TestSelenium(unittest.TestCase):
 
-    @pytest.mark.skipif(socket.gethostname() == "debian-server")
     def test_default(self) -> None:
         """"
         Test method to verify if selenium is working properly
         """
-        driver = self.driver_initializer()
+        if socket.gethostname() == "debian-server":
+            self.assertTrue(True, "Skipped because we are on the server")
+
+        driver = driver_initializer()
 
         driver.get("http://www.python.org")
 
@@ -25,12 +31,14 @@ class TestSelenium(unittest.TestCase):
 
         driver.close()
 
-    @pytest.mark.skipif(socket.gethostname() == "debian-server")
     def test_user_page(self) -> None:
         """"
         Test method to verify user page is working by checking its title
         """
-        driver = self.driver_initializer()
+        if socket.gethostname() == "debian-server":
+            self.assertTrue(True, "Skipped because we are on the server")
+
+        driver = driver_initializer()
 
         driver.get("https//38600.ddns.net:9001/utilisateurs")
 
@@ -38,14 +46,16 @@ class TestSelenium(unittest.TestCase):
 
         driver.close()
 
-    @pytest.mark.skipif(socket.gethostname() == "debian-server")
     def test_user_page_group_table(self) -> None:
         """"
         Test method to verify if the group list is working properly
         Here we verify if the size is equal to 2 because this fuctionnality is not yet
         implemented and the page is static
         """
-        driver = self.driver_initializer()
+        if socket.gethostname() == "debian-server":
+            self.assertTrue(True, "Skipped because we are on the server")
+
+        driver = driver_initializer()
 
         driver.get("https//38600.ddns.net:9001/utilisateurs")
 
@@ -53,25 +63,19 @@ class TestSelenium(unittest.TestCase):
         assert len(table_content) == 2
         driver.close()
 
-    @pytest.mark.skipif(socket.gethostname() == "debian-server")
     def test_admin_page_group_table(self) -> None:
         """"
         Test method to verify if the group list is working properly
         Here we verify if the size is equal to 2 because this functionality is not yet
         implemented and the page is static
         """
-        driver = self.driver_initializer()
+        if socket.gethostname() == "debian-server":
+            self.assertTrue(True, "Skipped because we are on the server")
+
+        driver = driver_initializer()
 
         driver.get("https//38600.ddns.net:9001/administrateurs")
 
         assert "Admin" in driver.title
 
         driver.close()
-
-
-    @pytest.mark.skipif(socket.gethostname() == "debian-server")
-    def driver_initializer(self):
-        s = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=s)
-        driver.maximize_window()
-        return driver
