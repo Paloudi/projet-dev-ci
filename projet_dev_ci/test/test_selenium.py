1d21a53243258a202fe711c38251ea292d50386c
Paloudi <quentinsb@free.fr>
2022-01-28 22:56:59 +0100

    updated selenium tests to be skipped on server

--- projet_dev_ci/test/test_selenium.py
+++ projet_dev_ci/test/test_selenium.py
@@ -1,19 +1,23 @@
+import socket
+
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import unittest
 
 
 class TestSelenium(unittest.TestCase):
 
+    @pytest.mark.skipif(socket.gethostname() == "debian-server")
     def test_default(self) -> None:
         """"
         Test method to verify if selenium is working properly
         """
-        driver = self.driver_intializer()
+        driver = self.driver_initializer()
 
         driver.get("http://www.python.org")
 
@@ -21,48 +25,52 @@ class TestSelenium(unittest.TestCase):
 
         driver.close()
 
+    @pytest.mark.skipif(socket.gethostname() == "debian-server")
     def test_user_page(self) -> None:
         """"
         Test method to verify user page is working by checking its title
         """
-        driver = self.driver_intializer()
+        driver = self.driver_initializer()
 
-        driver.get("http://127.0.0.1:8000/utilisateurs")
+        driver.get("https//38600.ddns.net:9001/utilisateurs")
 
         assert "User" in driver.title
 
         driver.close()
 
+    @pytest.mark.skipif(socket.gethostname() == "debian-server")
     def test_user_page_group_table(self) -> None:
         """"
         Test method to verify if the group list is working properly
         Here we verify if the size is equal to 2 because this fuctionnality is not yet
         implemented and the page is static
         """
-        driver = self.driver_intializer()
+        driver = self.driver_initializer()
 
-        driver.get("http://127.0.0.1:8000/utilisateurs")
+        driver.get("https//38600.ddns.net:9001/utilisateurs")
 
         table_content = driver.find_elements(By.CLASS_NAME, 'table-responsive')
         assert len(table_content) == 2
         driver.close()
 
+    @pytest.mark.skipif(socket.gethostname() == "debian-server")
     def test_admin_page_group_table(self) -> None:
         """"
         Test method to verify if the group list is working properly
         Here we verify if the size is equal to 2 because this functionality is not yet
         implemented and the page is static
         """
-        driver = self.driver_intializer()
+        driver = self.driver_initializer()
 
-        driver.get("http://127.0.0.1:8000/administrateurs")
+        driver.get("https//38600.ddns.net:9001/administrateurs")
 
         assert "Admin" in driver.title
 
         driver.close()
 
 
-    def driver_intializer(self):
+    @pytest.mark.skipif(socket.gethostname() == "debian-server")
+    def driver_initializer(self):
         s = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=s)
         driver.maximize_window()
